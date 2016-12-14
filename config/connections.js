647423e9cfd910a1e444620b7d49f5e10807536d
aayrton <hair.thon@gmail.com>
2016-12-14 01:49:54 +0100

    [dev] Create pet API

--- config/connections.js
+++ config/connections.js
@@ -40,7 +40,7 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  someMysqlServer: {
+  devMysqlServer: {
       adapter: 'sails-mysql',
       host: 'localhost',
       user: 'root', //optional
